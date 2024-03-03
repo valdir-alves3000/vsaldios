c0736411b2dc021da898c90cc6e6fee08615559e
valdir-alves3000 <valdiralves3000@gmail.com>
2024-03-03 10:16:12 -0300

    feat: adds function to fetch video information.

---
+++ web/src/fetchVideoInfo.js
@@ -0,0 +1,16 @@
+export async function fetchVideoInfo(url) {
+  try {
+    const response = await fetch(
+      `https://vsaldios.onrender.com/info?url=${url}`
+    );
+
+    if (!response.ok) {
+      console.error({ error: "Failed to fetch video information" });
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching video information:", error);
+  }
+}
